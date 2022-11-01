e23a447e33ff366c41e1e96fdce850e03806af6b
HiDana <holicdana@gmail.com>
2022-11-01 14:35:58 +0800

    build(webpack): set css module rule in webpack config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import _ from "lodash";
-import css from "css-loader!./style.css";
-
-function style(cssString) {
-  const element = document.createElement("style");
-  element.innerHTML = cssString;
-  return element;
-}
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
-  element.innerHTML = _.join(["Webpack", "hihi"], ": ");
+  element.innerHTML = _.join(["Webpack", "aaaa", "hihi"], ": ");
 
   return element;
 }
 
-document.body.appendChild(style(css.toString()));
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
       patterns: [{ from: path.resolve(__dirname, "public") }],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { modules: true } },
+        ],
+      },
+    ],
+  },
 };
